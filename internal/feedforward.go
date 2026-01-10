bf868aafd9d04ee4e5c1c633dd55b2b1a371df57
zakkbob <114868973+zakkbob@users.noreply.github.com>
2026-01-10 20:44:22 +0000

    Fix network bug

--- internal/feedforward.go
+++ internal/feedforward.go
@@ -1,5 +1,11 @@
 package internal
 
+import "math/rand"
+
+func preserve(n float64) float64 {
+	return n
+}
+
 func relu(n float64) float64 {
 	return max(0, n)
 }
@@ -8,6 +14,20 @@ type node struct {
 	processActivation func(float64) float64
 	weights           []float64
 	bias              float64
+}
+
+func randomNode(in int, activationFn func(float64) float64) node {
+	n := node{
+		processActivation: activationFn,
+		weights:           make([]float64, in),
+		bias:              rand.Float64(),
+	}
+
+	for i := range in {
+		n.weights[i] = rand.Float64()*2 - 1
+	}
+
+	return n
 }
 
 func (n *node) activate(in []float64) float64 {
@@ -28,11 +48,19 @@ type layer struct {
 	nodes []node
 }
 
-func (l *layer) process(in []float64) []float64 {
-	if len(in) != len(l.nodes) {
-		panic("layer's input value differs in length from its nodes")
+func randomLayer(in, out int, activationFn func(float64) float64) layer {
+	l := layer{
+		nodes: make([]node, out),
 	}
 
+	for i := range out {
+		l.nodes[i] = randomNode(in, activationFn)
+	}
+
+	return l
+}
+
+func (l *layer) process(in []float64) []float64 {
 	out := make([]float64, len(l.nodes))
 
 	for i, n := range l.nodes {
--- internal/feedforward_test.go
+++ internal/feedforward_test.go
@@ -1,10 +1,13 @@
 package internal
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestNode(t *testing.T) {
 	n := node{
-		processActivation: relu,
+		processActivation: preserve,
 		weights:           []float64{1, 2, 3},
 		bias:              4,
 	}
@@ -13,5 +16,52 @@ func TestNode(t *testing.T) {
 
 	if out != 21 {
 		t.Errorf("expected 21, got %v", out)
+	}
+}
+
+func TestNetwork(t *testing.T) {
+	n := network{
+		inputLen: 2,
+		layers: []layer{
+			{
+				nodes: []node{
+					{
+						processActivation: preserve,
+						weights:           []float64{1, 2},
+						bias:              4,
+					},
+					{
+						processActivation: preserve,
+						weights:           []float64{0.5, 0.25},
+						bias:              -2,
+					},
+					{
+						processActivation: preserve,
+						weights:           []float64{-1, 2},
+						bias:              1,
+					},
+				},
+			},
+			{
+				nodes: []node{
+					{
+						processActivation: preserve,
+						weights:           []float64{-2, 4, 0.5},
+						bias:              -4,
+					},
+					{
+						processActivation: preserve,
+						weights:           []float64{-3, -0.5, 2},
+						bias:              3,
+					},
+				},
+			},
+		},
+	}
+
+	out := n.process([]float64{-1, 4})
+
+	if !slices.Equal(out, []float64{-27, -9.25}) {
+		t.Errorf("expected [-27, -9.25], got %v", out)
 	}
 }
