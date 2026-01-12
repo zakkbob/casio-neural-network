5c6b9d1456827f96fbb1a49aa2188b4789c1c3ac
zakkbob <114868973+zakkbob@users.noreply.github.com>
2026-01-12 23:22:31 +0000

    Remove brittle tests

--- internal/feedforward_test.go
+++ internal/feedforward_test.go
@@ -1,64 +1,8 @@
 package internal
 
 import (
-	"slices"
 	"testing"
 )
-
-func TestNode(t *testing.T) {
-	n := node{
-		weights: []float64{1, 2, 3},
-		bias:    4,
-	}
-
-	out := n.predict([]float64{3, 1, 4})
-
-	if out != 21 {
-		t.Errorf("expected 21, got %v", out)
-	}
-}
-
-func TestNetwork(t *testing.T) {
-	n := network{
-		inputLen: 2,
-		layers: []*layer{
-			{
-				nodes: []node{
-					{
-						weights: []float64{1, 2},
-						bias:    4,
-					},
-					{
-						weights: []float64{0.5, 0.25},
-						bias:    2,
-					},
-					{
-						weights: []float64{1, 2},
-						bias:    1,
-					},
-				},
-			},
-			{
-				nodes: []node{
-					{
-						weights: []float64{2, 4, 0.5},
-						bias:    4,
-					},
-					{
-						weights: []float64{3, -0.5, 2},
-						bias:    3,
-					},
-				},
-			},
-		},
-	}
-
-	out := n.predict([]float64{-1, 4})
-
-	if !slices.Equal(out, []float64{-27, -9.25}) {
-		t.Errorf("expected [-27, -9.25], got %v", out)
-	}
-}
 
 func TestTrain(t *testing.T) {
 	n := network{
@@ -78,9 +22,11 @@ func TestTrain(t *testing.T) {
 		{0.1, 0.15},
 		{0.5, 0.35},
 		{0.7, 0.45},
-	}, 1000)
+	}, 10000)
 
 	out := n.predict([]float64{0.3})
+
+	t.Log(out)
 
 	if !(out[0]-0.3 < 0.1 && out[1]-0.25 < 0.1) {
 		t.Errorf("expected close to [0.3, 0.25], got %v", out)
