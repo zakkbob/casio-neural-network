c65229be6b41dfacf949bb9b5ab93a3ef91a0dc9
zakkbob <114868973+zakkbob@users.noreply.github.com>
2026-01-14 15:34:54 +0000

    wip

--- internal/feedforward.go
+++ internal/feedforward.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"math/rand"
 	"slices"
+	"fmt"
 )
 
 func identity(n float64) float64 {
@@ -113,37 +114,82 @@ func (n *network) train(ins [][]float64, outs [][]float64, passes int) {
 	for range passes {
 		old := n.clone()
 
+		losses := []float64{}
+
 		for i, in := range ins {
 			out := outs[i]
 
 			old.predict(in)
 
+			lastLosses := []float64{}
+
 			outputLayer := old.layers[len(n.layers)-1]
 			for j := range outputLayer.nodes { // output layer
 				output := outputLayer.lastOutputs[j]
 
-				if output <= 0 { // ReLU dead thing
+				loss := output - out[j]
+				lastLosses = append(lastLosses, loss)
+
+if output <= 0 { // ReLU dead thing
 					continue
 				}
 
-				error := output - out[j]
-
 				if len(n.layers) > 1 {
 					for k := range n.layers[len(n.layers)-2].nodes {
-						gradient := old.layers[len(n.layers)-2].lastOutputs[k] * error
+						gradient := old.layers[len(n.layers)-2].lastOutputs[k] * loss
 
-						n.layers[len(n.layers)-1].nodes[j].weights[k] += -alpha * gradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point error
+						n.layers[len(n.layers)-1].nodes[j].weights[k] += -alpha * gradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point loss
 					}
 				} else {
 					for k := range n.inputLen {
-						gradient := in[k] * error
+						gradient := in[k] * loss
 
-						n.layers[len(n.layers)-1].nodes[j].weights[k] += -alpha * gradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point error
+						n.layers[len(n.layers)-1].nodes[j].weights[k] += -alpha * gradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point loss
 					}
 				}
 
-				biasGradient := outputLayer.nodes[j].bias * error
-				n.layers[len(n.layers)-1].nodes[j].bias += -alpha * biasGradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point error
+				biasGradient := outputLayer.nodes[j].bias * loss
+				n.layers[len(n.layers)-1].nodes[j].bias += -alpha * biasGradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point loss
+			}
+
+			for l := len(n.layers) - 2; l >= 0; l-- { // hidden layers
+				layer := old.layers[l]
+				losses = []float64{}
+
+				for j := range layer.nodes { // output layer
+					output := layer.lastOutputs[j]
+
+					if output <= 0 { // ReLU dead thing
+						continue
+					}
+
+					fmt.Println(lastLosses)
+					loss := output
+					for o := range n.layers[l+1].nodes {
+						loss -= n.layers[l+1].nodes[o].weights[j] * lastLosses[o]
+					}
+
+					losses = append(losses, loss)
+
+					if l != 0 {
+						for k := range n.layers[l-1].nodes {
+							gradient := old.layers[l-1].lastOutputs[k] * loss
+
+							n.layers[l].nodes[j].weights[k] += -alpha * gradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point loss
+						}
+					} else {
+						for k := range n.inputLen {
+							gradient := in[k] * loss
+
+							n.layers[l].nodes[j].weights[k] += -alpha * gradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point loss
+						}
+					}
+
+					biasGradient := layer.nodes[j].bias * loss
+					n.layers[l].nodes[j].bias += -alpha * biasGradient / float64(len(ins)) // divide by the length of ins, removes need to sum and calculate mean after. probably increases floating point loss
+				}
+
+				lastLosses = losses
 			}
 		}
 	}
--- internal/feedforward_test.go
+++ internal/feedforward_test.go
@@ -5,30 +5,69 @@ import (
 )
 
 func TestTrain(t *testing.T) {
-	n := network{
-		inputLen: 1,
-		layers: []*layer{
-			randomLayer(1, 2, relu),
-		},
-	}
+	t.Run("One Layer", func(t *testing.T) {
+		n := network{
+			inputLen: 1,
+			layers: []*layer{
+				randomLayer(1, 2, relu),
+			},
+		}
 
-	n.train([][]float64{
-		{0},
-		{0.1},
-		{0.5},
-		{0.7},
-	}, [][]float64{
-		{0, 0.1}, // n, 0.5n+0.1
-		{0.1, 0.15},
-		{0.5, 0.35},
-		{0.7, 0.45},
-	}, 10000)
+		n.train([][]float64{
+			{0},
+			{0.1},
+			{0.5},
+			{0.7},
+		}, [][]float64{
+			{0, 0.1}, // n, 0.5n+0.1
+			{0.1, 0.15},
+			{0.5, 0.35},
+			{0.7, 0.45},
+		}, 10000)
 
-	out := n.predict([]float64{0.3})
+		out := n.predict([]float64{0.3})
 
-	t.Log(out)
+		t.Log(out)
 
-	if !(out[0]-0.3 < 0.1 && out[1]-0.25 < 0.1) {
-		t.Errorf("expected close to [0.3, 0.25], got %v", out)
-	}
+		if !(out[0]-0.3 < 0.1 && out[1]-0.25 < 0.1) {
+			t.Errorf("expected close to [0.3, 0.25], got %v", out)
+		}
+	})
+
+	t.Run("Two Layers", func(t *testing.T) {
+		n := network{
+			inputLen: 3,
+			layers: []*layer{
+				randomLayer(3, 3, relu),
+				randomLayer(3, 2, relu),
+			},
+		}
+
+		n.train([][]float64{
+			{1, 0, 0},
+			{1, 0, 1},
+			{0, 1, 0},
+			{0, 1, 1},
+			{1, 1, 1},
+			{1, 1, 0},
+		}, [][]float64{
+			{1, 0},
+			{1, 0},
+			{0, 1},
+			{0, 1},
+			{1, 0},
+			{1, 0},
+		}, 10000)
+
+		out := n.predict([]float64{1, 1, 0})
+
+		t.Log(out)
+		t.Log(n.layers[0].nodes)
+		t.Log(n.layers[1].nodes)
+
+		if !(1-out[0] < 0.1) {
+			t.Errorf("expected close to 1, got %v", out)
+		}
+	})
+
 }
